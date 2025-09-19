39f85f72a11924fa640f2d223d261090ae5fa663
Marion <marionbailleux07@gmail.com>
2025-09-19 09:29:31 +0200

    type params as promise

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import BurgerItem from "@/types/burgerItem";
 import { notFound } from "next/navigation";
 
-const Product = async ({ params }: { params: { id: string } }) => {
+const Product = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   const products = await getProducts();
   const product = products.items.find((item: BurgerItem) => item.id === id);
